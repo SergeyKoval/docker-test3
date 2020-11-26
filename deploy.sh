cb57bc62f994f8f2eab90a3beedd2c152b15d944
Sergey Koval <skoval@exadel.com>
2020-11-26 11:33:43 +0300

    Added deployments scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t deplake/multi-client:latest -t deplake/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t deplake/multi-server:latest -t deplake/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t deplake/multi-worker:latest -t deplake/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push deplake/multi-client:latest
+docker push deplake/multi-server:latest
+docker push deplake/multi-worker:latest
+
+docker push deplake/multi-client:$SHA
+docker push deplake/multi-server:$SHA
+docker push deplake/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployments server=deplake/multi-server:$SHA
+kubectl set image deployments/client-deployments client=deplake/multi-client:$SHA
+kubectl set image deployments/worker-deployments worker=deplake/multi-worker:$SHA
